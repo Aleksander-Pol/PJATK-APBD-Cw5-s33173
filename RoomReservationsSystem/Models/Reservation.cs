64c8d382a5f70d89edf8a6252b38a61917e130ef
Aleksander Polanski <s33173@pjwstk.edu.pl>
2026-04-15 00:02:01 +0200

    Added data annotations except one connected to EndTime

--- RoomReservationsSystem/Models/Reservation.cs
+++ RoomReservationsSystem/Models/Reservation.cs
@@ -1,4 +1,5 @@
-﻿using RoomReservationsSystem.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using RoomReservationsSystem.Enums;
 
 namespace RoomReservationsSystem.Models;
 
@@ -6,10 +7,15 @@ public class Reservation
 {
     public int Id { get; set; }
     public int RoomId { get; set; }
+    
+    [Required]
     public string OrganizerName { get; set; }
+   
+    [Required]
     public string Topic { get; set; }
     public DateOnly Date{ get; set; }
     public TimeOnly StartTime { get; set; } 
+    
     public TimeOnly EndTime { get; set; }
     public Status Status { get; set; }
 }
--- RoomReservationsSystem/Models/Room.cs
+++ RoomReservationsSystem/Models/Room.cs
@@ -1,11 +1,18 @@
-﻿namespace RoomReservationsSystem.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoomReservationsSystem.Models;
 
 public class Room
 {
     public int Id { get; set; }
+    
+    [Required]
     public string Name { get; set; }
+    [Required]
     public string BuildingCode { get; set; }
     public int Floor { get; set; }
+    
+    [Range(0, int.MaxValue, ErrorMessage = "Capacity has to be positive")]
     public int Capacity { get; set; }
     public bool HasProjector { get; set; }
     public bool IsActive { get; set; }
