1a05213a6b16711a7ddad19ebe5620c1786c8f9f
Aleksander Polanski <s33173@pjwstk.edu.pl>
2026-04-14 13:56:03 +0200

    Reservation class

--- RoomReservationsSystem/Controllers/ReservationsController.cs
+++ RoomReservationsSystem/Controllers/ReservationsController.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RoomReservationsSystem.Enums;
 
 namespace RoomReservationsSystem.Controllers;
 
 public class ReservationsController : ControllerBase
 {
-    
+    public int Id { get; set; }
+    public string RoomId { get; set; }
+    public string OrganizerName { get; set; }
+    public string Topic { get; set; }
+    public DateOnly Date{ get; set; }
+    public TimeOnly StartTime { get; set; } 
+    public TimeOnly EndTime { get; set; }
+    public Status Status { get; set; }
 }
---
+++ RoomReservationsSystem/Enums/Status.cs
@@ -0,0 +1,6 @@
+﻿namespace RoomReservationsSystem.Enums;
+
+public enum Status
+{
+    Planned, Confirmed, Cancelled
+}
