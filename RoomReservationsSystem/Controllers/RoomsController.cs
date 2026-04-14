f2ff5804a6c75bccb2333d14fd04a4b698c6297e
Aleksander Polanski <s33173@pjwstk.edu.pl>
2026-04-14 17:08:21 +0200

    Added HttpGet methods to RoomsController: getRooms(), GetRoomById, GetRoomsByBuildingCode

--- RoomReservationsSystem/Controllers/RoomsController.cs
+++ RoomReservationsSystem/Controllers/RoomsController.cs
@@ -1,8 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using RoomReservationsSystem.Models;
 
 namespace RoomReservationsSystem.Controllers;
 
+[ApiController]
+[Route("api/rooms")]
 public class RoomsController : ControllerBase
 {
+    public static List<Room> Rooms =
+    [
+        new Room()
+        {
+            Id = 1,
+            Name = "Mordor",
+            BuildingCode = "36a",
+            Capacity = 64,
+            Floor = 4,
+            HasProjector = true,
+            IsActive = false
+        },
+        new Room()
+        {
+            Id = 2,
+            Name = "Jungle",
+            BuildingCode = "36a",
+            Capacity = 46,
+            Floor = 7,
+            HasProjector = false,
+            IsActive = true
+        }
+    ];
+        
     
+    [HttpGet]
+    public IActionResult GetRooms()
+    {
+        return Ok(Rooms);
+    }
+
+    [HttpGet("{Id:int}")]
+    public IActionResult GetRoomById(int id)
+    {
+        var room = Rooms.FirstOrDefault(r => r.Id == id);
+        return Ok(room);
+    }
+
+    [HttpGet("building/{buildingCode}")]
+    public IActionResult GetRoomsByBuildingCode(string buildingCode)
+    {
+        var rooms = Rooms.FindAll(r => r.BuildingCode.Equals(buildingCode));
+        return Ok(rooms);
+    }
 }
